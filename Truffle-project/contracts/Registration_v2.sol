ddef9dc64244981040d3debe56edf42d835c6586
merkol <merterkol@outlook.com>
2022-11-30 20:05:44 +0300

    Admin for loop update

--- Truffle-project/contracts/Registration_v2.sol
+++ Truffle-project/contracts/Registration_v2.sol
@@ -14,9 +14,16 @@ contract Registration_v2{
     }
 
 
-    address adminAddr = 0x2dBAf1bD7109718271527a74524879fa3B54103E;
-
-    function sendCredentials(uint signature, address _adminAddr) public view{
-        require(address(_adminAddr)==address(adminAddr) , "Receiver is NOT Admin");
-    }   
+    address[] adminAddr = [0x2dBAf1bD7109718271527a74524879fa3B54103E,0x3c68F2cf6Fb146787e1632CA569Fcbd5f1dE9AfC,
+                            0x6d15a4B9B1c734624f4f73bB23CC643a34bAA19f, 0xFaa15bb6bcb2F045Ce6E0cF4aabb858b93330F9b]
+;
+    function sendCredentials(uint signature, address _adminAddr) public {
+        bool adminCondition = false;
+        for(uint i = 0; i < adminAddr.length; i++){
+            if (adminAddr[i] == _adminAddr){
+                adminCondition = true;
+            }
+        }   
+        require(adminCondition == true , "Receiver is NOT Admin");
+    }
 }
