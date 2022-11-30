8ea08c4bcd5883765fbbce8ff51e671e09f745cf
atakrk <atakuruk@gmail.com>
2022-11-30 18:57:36 +0300

    deneme

--- Truffle-project/migrations/2_deploy_contracts.js
+++ Truffle-project/migrations/2_deploy_contracts.js
@@ -3,9 +3,9 @@ const Authentication = artifacts.require("Authentication");
 const Registration_v2 = artifacts.require("Registration_v2");
 //const Storage = artifacts.require("Storage");
 module.exports = function(deployer) {
-
   deployer.deploy(Registration);
   deployer.deploy(Authentication);
   deployer.deploy(Registration_v2);
+  // deneme
   //deployer.deploy(Storage);
 };
